367d3f515a07d1da78b5e2bc23e0fd31e1abe38f
Jean Max S Krebs <jeanmaxskrebs@gmail.com>
2021-03-04 14:19:26 -0300

    ATT dados

---
+++ index.js
@@ -0,0 +1,11 @@
+function idade() {
+    dia = 01;
+    mes = 03;
+    ano = 2001;
+    let idade;
+    idade = new Date().getFullYear() - ano;
+    HTMLNovo = document.createTextNode(idade+" anos | Estudante TSI - Ifsul Pelotas");
+
+    let a = document.getElementById("idade");
+    a.appendChild(HTMLNovo);
+}
